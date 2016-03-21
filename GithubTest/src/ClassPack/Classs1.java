2ce05c13bdb33c0b2a833c841f1e09ef8e8b5b31
RawatJ <jaishreenegi@gmail.com>
2016-03-21 14:32:51 -0700

    nothing much at 2:32

--- GithubTest/src/ClassPack/Classs1.java
+++ GithubTest/src/ClassPack/Classs1.java
@@ -4,7 +4,10 @@ public class Classs1 {
 	public static void main(String[] args) {
 		System.out.println("I am class one");
 	System.out.print("edited");
-	
+	System.out.print("Printed through Bash.");
+
+
+	System.out.print("This is second bash updation.")
 	}
 
 }
