39147b85702becc1849dd152f213857f9349c7e1
RawatJ <jaishreenegi@gmail.com>
2016-03-21 14:35:54 -0700

    commiting from ecplise

--- GithubTest/src/ClassPack/Classs1.java
+++ GithubTest/src/ClassPack/Classs1.java
@@ -4,7 +4,12 @@ public class Classs1 {
 	public static void main(String[] args) {
 		System.out.println("I am class one");
 	System.out.print("edited");
+	display();
+	}
 	
+	public static void display(){
+		
+		System.out.println("Display");
 	}
 
 }
